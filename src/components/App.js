dae0457283e6ef4191daac5490e24baea49549dd
Alfredo Rafael Fernández Zamora <alfredo.fernandez@wizeline.com>
2020-04-30 23:19:48 -0500

    add 404 page

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,20 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import HomePage from "./home/HomePage";
 import AboutPage from "./about/AboutPage";
 import Header from "./common/Header";
+import NotFoundPage from "./NotFoundPage";
 
 // <> or <React.Fragment> is used to wrap elements-components without create a div or other tag in the DOM.
 const App = () => (
   <>
     <Header />
     <div className="container-fluid">
-      <Route exact path="/" component={HomePage} />
-      <Route path="/about" component={AboutPage} />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/about" component={AboutPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
     </div>
   </>
 );
---
+++ src/components/NotFoundPage.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const NotFoundPage = () => <h1>Ooops! Page not found.</h1>;
+
+export default NotFoundPage;
